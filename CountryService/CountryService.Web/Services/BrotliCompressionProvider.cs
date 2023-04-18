7661503fa261fb19b8f820991ba6d1a63e166228
LourenceQ <lawrenceqf@gmail.com>
2023-04-17 23:03:35 -0300

    Implementando compression providers

---
+++ CountryService/CountryService.Web/Services/BrotliCompressionProvider.cs
@@ -0,0 +1,36 @@
+﻿using Grpc.Net.Compression;
+using System.IO.Compression;
+
+namespace CountryService.Web.Services;
+
+public class BrotliCompressionProvider : ICompressionProvider
+{
+    private readonly CompressionLevel? _compressionLevel;
+    public string EncodingName => "br";
+
+    public BrotliCompressionProvider(CompressionLevel? compressionLevel)
+    {
+        _compressionLevel = compressionLevel;
+    }
+    public BrotliCompressionProvider()
+    {
+
+    }
+
+    public Stream CreateCompressionStream(Stream stream, CompressionLevel? compressionLevel)
+    {
+        if (_compressionLevel.HasValue)
+            return new BrotliStream(stream, compressionLevel ?? _compressionLevel.Value, true);
+
+        else if (!_compressionLevel.HasValue && compressionLevel.HasValue)
+            return new BrotliStream(stream, compressionLevel.Value, true);
+
+        return new BrotliStream(stream, CompressionLevel.Fastest, true);
+
+    }
+
+    public Stream CreateDecompressionStream(Stream stream)
+    {
+        return new BrotliStream(stream, CompressionMode.Decompress);
+    }
+}
